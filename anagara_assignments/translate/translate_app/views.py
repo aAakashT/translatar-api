9d48581bddb84a507c40b8dd2743264974fc6016
aAakashT <127603440+aAakashT@users.noreply.github.com>
2023-08-13 21:28:51 +0530

    Update views.py

--- anagara_assignments/translate/translate_app/views.py
+++ anagara_assignments/translate/translate_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from googletrans import Translator
+#from googletrans import Translator
 from .utils import TranslationCache
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
